d698cc79301ac7e7de7440cc1aeb70ce0eefe96f
samkeen <sam.keen@janrain.com>
2014-01-04 10:51:47 -0800

    brought in body parsing and env specific setting + added a config file stub

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 var express = require("express");
 var app = express();
 
+var config = require("./config");
+
+// all environments
+app.configure(function () {
+  app.set("config", config);
+  app.use(express.logger("dev"));// Output development-friendly colored logs
+  // body parsing
+  app.use(express.json());
+  app.use(express.urlencoded());
+  //  app.use(express.multipart());
+
+
+});
+
+// development only
+app.configure("development", function () {
+  app.use(express.errorHandler());
+});
+
+// Production only
+app.configure("production", function () {
+  app.use(express.errorHandler({dumpExceptions: true}));
+});
+
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
+
 // long hand
 app.get("/", function(req, res){
   var body = "Hello World";
@@ -12,6 +42,17 @@ app.get("/", function(req, res){
 // express short hand
 app.get("/short", function(req, res){
   res.send("Hello World");
+});
+
+/**
+ * Display routes if in development env
+ */
+app.get("/routes", function(req, res){
+  if('development' == app.get('env')) {
+    res.send(app.routes);
+  } else {
+    res.send(404);
+  }
 });
 
 app.listen(3003);
